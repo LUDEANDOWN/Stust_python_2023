9f3b85e3b18e6fee4e6f5c94e2a7b1954f24c01a
劉定宗 <91250803+LUDEANDOWN@users.noreply.github.com>
2023-12-07 11:43:12 +0800

    2

--- esercsie_1207/class_Sports.py
+++ esercsie_1207/class_Sports.py
@@ -1,34 +1,73 @@
-class Fruit:
-    def __init__(self,name:str):
+class Sports:
+    def __init__(self,name):
         self._name=name
+
     @property
-
-    def fruit_name(self):
-        print(f'"{self._name}" was accessed.')
+    def sports_name(self):
         return self._name
-
-    @fruit_name.setter
-    def fruit_name(self,value):
-        print(f'"{self._name}" is now "{value}"')
+        
+    @sports_name.setter
+    def sports_name(self,value):
         self._name=value
 
-    @fruit_name.deleter
-    def fruit_name(self):
-        print(f'"{self._name}" was deleted')
-        del self._name
+    def practice(self):
+        print_str = "Doing Sports practice"
+        return print_str
+
+class LandSports(Sports):
+    def __init__(self, name,field):
+        super().__init__(name)
+        self._field=field
+
+    @property
+    def landsports_field(self):
+        return self._field
+
+    def practice(self):
+        print_str = "Doing Land Sports practice"
+        return print_str
 
 
-
-if __name__=='__main__':
-    fruit=Fruit('Banana')
-
-    print(fruit.fruit_name)
-
-    fruit.fruit_name='apple'
-
-    print(fruit.fruit_name)
-
-    del fruit.fruit_name 
-
-    print(fruit.fruit_name)
+class WaterSports(Sports):
+    def __init__(self, name,activity):
+        super().__init__(name)
+        self._activity=activity
+    @property
+    def watersports_activity(self):
+        return self._activity
     
+    def practice(self):
+        print_str = "Doing Water Sports practice"
+        return print_str
+
+class BallSports(LandSports):
+    def __init__(self, name):
+        super().__init__(name)
+
+class NonBallSports(LandSports):
+    def __init__(self, name):
+        super().__init__(name)
+
+class PoolSports(WaterSports):
+    def __init__(self, name):
+        super().__init__(name)
+
+class SeaSports(WaterSports):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+if __name__=="__main__":
+    baseball=LandSports("baseball", "diamond field")
+    print(baseball.sports_name)
+    print(baseball.landsports_field)
+    print(baseball.practice())
+
+    water_sliing=WaterSports("Water Skiing","Ride the waves and feel the splash of water on your face as you carve through the surf.")
+    print(water_sliing.sports_name)
+    print(water_sliing.watersports_activity)
+    print(water_sliing.practice())
+
+    sports=Sports("Softball")
+    print(sports.sports_name)
+    print(sports.practice())
